2e14e10b450e3225a35a602045a2e2cc52f81666
OMAR <rockstonestudios666@gmail.com>
2025-06-12 22:52:55 -0500

    Implementacion de movimiento del jugador e IA de enemigos

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,40 @@
+
+
+using StarterAssets;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] int damageAmount = 1;
+    StarterAssetsInputs starterAssetsInputs;
+
+
+    void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    }
+
+    void Update()
+    {
+        HandleShhot();
+    }
+    
+
+    private void HandleShhot()
+    {
+        if (!starterAssetsInputs.shoot) return;
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+            {
+                EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+                enemyHealth?.TakeDamage(damageAmount);
+
+                starterAssetsInputs.ShootInput(false);
+
+            }
+        }
+
+
+    }
+}
